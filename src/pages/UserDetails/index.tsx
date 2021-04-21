9a393f5c68975d67c992fd232a5001196c0ffcae
bruceji7 <revolverc24@gmail.com>
2021-04-21 15:45:25 +0900

    Add: Hover and clicking on profile pic opens file input window

--- src/pages/UserDetails/index.tsx
+++ src/pages/UserDetails/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { useQuery } from "react-query";
-import instance from "../../axios/instance";
-
 import Button from "../../components/Button";
 
 import FileCase from "./FileCase";
@@ -11,6 +8,8 @@ import FileCase from "./FileCase";
 import styles from "./UserDetails.module.scss";
 import BackIcon from "../../assets/icons/back.svg";
 import UserInfo from "./UserInfo";
+import useMyUploads from "../../hooks/useMyUploads";
+import useMyDownloads from "../../hooks/useMyDownloads";
 
 const LIMIT = "3";
 
@@ -29,27 +28,8 @@ function BackButton() {
 function UserDetails() {
   const authorID = "17";
 
-  const { data: uploadsData } = useQuery(["uploads", authorID], async () => {
-    const formData = new FormData();
-    formData.append("author", authorID);
-    formData.append("order_by", "rate");
-    formData.append("limit", LIMIT);
-    const { data } = await instance.post("/content/get-contents-by", formData);
-    return data.data;
-  });
-
-  const { data: downloadsData, error } = useQuery(
-    ["downloads", authorID],
-    async () => {
-      console.log("/content/downloaded-by/" + authorID + "?limit=" + LIMIT);
-      const { data } = await instance.get(
-        `/content/downloaded-by/${authorID}?limit=${LIMIT}`
-      );
-      return data.data;
-    }
-  );
-
-  console.log(error);
+  const { data: uploadsData } = useMyUploads({ authorID, LIMIT });
+  const { data: downloadsData } = useMyDownloads({ authorID, LIMIT });
 
   return (
     <div className={styles.Background}>
