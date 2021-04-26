a8bd66b318063439212a134b8c702fb76758cae0
Ivan Saldano <ivanms1@gmail.com>
2021-04-26 15:28:29 +0900

    add avatar upload

--- src/pages/UserDetails/ProfilePicture/index.tsx
+++ src/pages/UserDetails/ProfilePicture/index.tsx
@@ -1,9 +1,17 @@
 import React, { useRef, useState } from "react";
+import classNames from "classnames";
+import { useMutation } from "react-query";
+
 import useGetImage from "../../../hooks/useGetImage";
+import useCurrentUser from "../../../hooks/useCurrentUser";
+
+import instance from "../../../axios/instance";
 
 import NoAvatar from "../..//../assets/icons/no-avatar.svg";
 
 import styles from "./ProfilePicture.module.scss";
+
+const { api } = window;
 
 type ProPicProps = {
   children: React.ReactNode;
@@ -37,6 +45,7 @@ function ProPic({
       </div>
     );
   }
+
   return (
     <NoAvatar
       className={styles.ProPic}
@@ -60,14 +69,30 @@ function ProfilePicture({
   avatar: string;
   isSelf: boolean;
 }) {
-  const inputRef = useRef(null);
-
+  const { currentUser, refetch } = useCurrentUser();
   const { data: avatarImgSrc } = useGetImage(avatar);
 
   const [msgShow, setMsgShow] = useState<boolean>(true);
-  function handleEditPic() {
+
+  const { mutateAsync: uploadAvatar } = useMutation(async (hash: string) => {
+    const formData = new FormData();
+    formData.append("avatar", hash);
+    const { data } = await instance.put(`/user/${currentUser?.id}`, formData);
+    return data;
+  });
+
+  const inputRef = useRef(null);
+
+  const handleEditPic = () => {
     inputRef.current.click();
-  }
+  };
+
+  const handleAvatarUpload = async (e) => {
+    const avatarToUpload = e.target.files[0];
+    const data = await api.uploadAvatar(avatarToUpload?.path);
+    await uploadAvatar(data?.hash);
+    refetch();
+  };
 
   if (isSelf === true) {
     return (
@@ -78,13 +103,20 @@ function ProfilePicture({
           setMsgShow={setMsgShow}
         >
           <span
-            className={styles.EditMessage}
-            style={{ visibility: msgShow ? "visible" : "hidden" }}
+            className={classNames(styles.EditMessage, {
+              [styles.show]: msgShow,
+            })}
           >
             Edit
           </span>
         </ProPic>
-        <input type="file" ref={inputRef} style={{ display: "none" }} />
+        <input
+          type="file"
+          onChange={handleAvatarUpload}
+          className={styles.HiddenInput}
+          ref={inputRef}
+          accept="image/x-png,image/gif,image/jpeg"
+        />
       </div>
     );
   }
