bc67054609877c29a65eac5d98a2d6342e25f5a0
bruceji7 <revolverc24@gmail.com>
2021-04-26 14:43:45 +0900

    Minor: Import order in similarCell

--- src/pages/FileDetails/SimilarProducts/SimilarCell/index.tsx
+++ src/pages/FileDetails/SimilarProducts/SimilarCell/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import trunc from "../../../../helpers/trunc";
 
 import useGetImage from "../../../../hooks/useGetImage";
+
+import trunc from "../../../../helpers/trunc";
 
 import { FileProps } from "../../../../types";
 
