fbfd3fc1e50dc5f25a7efe8512893c17ab5e2742
Ivan Saldano <ivanms1@gmail.com>
2021-05-14 15:17:13 +0900

    remove bootstrap

--- electron/ipfs/index.ts
+++ electron/ipfs/index.ts
@@ -1,18 +1,8 @@
 import IPFS from "ipfs-core";
-import Protector from "libp2p/src/pnet";
-import fs from "fs";
-import PeerId from "peer-id";
-import { join } from "path";
 
 import logger from "../logger";
 
 let node = null;
-
-const BOOTSTRAP_ADDRESSS_1 =
-  "/ip4/52.79.200.55/tcp/4001/ipfs/12D3KooWFyYb19Xki7pj4PyQ1jnZsEx4MfExyng2MZCAtpPXoCxb";
-
-const BOOTSTRAP_ADDRESSS_2 =
-  "/ip4/3.34.181.64/tcp/4002/ipfs/12D3KooWRRxEVRbptGzX4Moz6xEqrGnCA35kvFW371QwFuSipwEJ";
 
 export function getIpfs() {
   return node;
@@ -20,28 +10,7 @@ export function getIpfs() {
 
 export async function createIpfs() {
   try {
-    const privateKey = await PeerId.create({ keyType: "Ed25519" });
-
-    logger(
-      "swarm-key",
-      `swarm Key: ${fs.readFileSync(join(__dirname, "../assets/swarm.key"))}`,
-      "info"
-    );
-
-    node = await IPFS.create({
-      libp2p: {
-        modules: {
-          connProtector: new Protector(
-            fs.readFileSync(join(__dirname, "../assets/swarm.key"))
-          ),
-        },
-      },
-      // @ts-expect-error
-      config: {
-        Bootstrap: [BOOTSTRAP_ADDRESSS_1, BOOTSTRAP_ADDRESSS_2],
-      },
-      init: { privateKey },
-    });
+    node = await IPFS.create();
 
     const id = await node.id();
     const peers = await node.swarm.peers();
