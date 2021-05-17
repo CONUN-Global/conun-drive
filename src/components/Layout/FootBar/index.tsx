8abba4c2a32e7385dfcce4e3e57019f89b3cfd58
bruceji7 <revolverc24@gmail.com>
2021-05-17 15:03:35 +0900

    Add: Footer that displays version /update status

---
+++ src/components/Layout/FootBar/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useGetAppVersion from "../../../hooks/useGetAppVersion";
+import useGetLatestRelease from "../../../hooks/useGetLatestRelease";
+import styles from "./FootBar.module.scss";
+
+function FootBar() {
+  const { version: currentAppVersion } = useGetAppVersion();
+  const { latestVersion } = useGetLatestRelease();
+
+  console.log(latestVersion);
+
+  return (
+    <div className={styles.FootBar}>
+      {!currentAppVersion && (
+        <>
+          <span className={styles.CurrentVersion}>
+            VERSION {currentAppVersion}
+          </span>
+          {currentAppVersion !== latestVersion ? (
+            <span className={styles.UpdateStatus}>
+              A new version is available!
+            </span>
+          ) : (
+            <span className={styles.UpdateStatus}>All up-to-date!</span>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default FootBar;
