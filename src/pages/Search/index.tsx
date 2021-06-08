ef318a6a33097a71240c36e343c0e714d52c2dce
bruceji7 <revolverc24@gmail.com>
2021-06-08 18:01:01 +0900

    Fix: Page sizing and limit was blocking pagination query, fixed

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -14,7 +14,7 @@ import { FileProps } from "../../types";
 
 import styles from "./Search.module.scss";
 
-const PAGE_LIMIT = 10;
+const PAGE_LIMIT = 18;
 
 function Search() {
   const location = useLocation();
@@ -34,7 +34,7 @@ function Search() {
       const { data } = await instance.get(
         `/search/content?keyword=${query.get("keyword")}&filter=${query.get(
           "filter"
-        )}&page=${pageParam}`
+        )}&page=${pageParam}&limit=${PAGE_LIMIT}`
       );
 
       total.current = data?.data?.total;
@@ -66,7 +66,9 @@ function Search() {
         {(files?.pages || []).map((group, i) => (
           <React.Fragment key={i}>
             {group?.data?.map((file: FileProps) => (
-              <FileBox key={file.id} file={file} />
+              <div key={file.id} className={styles.Cell}>
+                <FileBox file={file} />
+              </div>
             ))}
           </React.Fragment>
         ))}
